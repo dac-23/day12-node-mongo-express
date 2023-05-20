6aaaed5dba49318261551e03de2f1100ed03c2f1
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2023-05-20 10:32:20 +0530

    static path

--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
 import express from "express";
-
-// Initializing the express
 const app = express();
 
-// Starting the server
+// Handling the Client Request
+// http://localhost:4000/
+app.get("/", function (req, res) {
+  res.json({});
+});
+
+// http://localhost:4000/users
+app.get("/users/", (req, res) => {
+  let list = [];
+  let user1 = { id: 1, name: "rohit", email: "rohit@gmail.com" };
+  let user2 = { id: 2, name: "rahul", email: "rahul@gmail.com" };
+  list.push(user1);
+  list.push(user2);
+
+  res.json(list);
+});
+
+// http://localhost:4000/users/1
+app.get("/users/1", (req, res) => {
+  let user = {
+    id: 1,
+    name: "rohit",
+    email: "rohit@gmail.com",
+  };
+
+  res.json(user);
+});
+
 app.listen(4000);
