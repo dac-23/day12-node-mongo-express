465982814cec4b02c98abd6d4406e95b2c8eccc1
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2023-05-20 12:14:15 +0530

    dlete one.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { MongoClient, ObjectId } from "mongodb";
 import express from "express";
 import cors from "cors";
 const app = express();
@@ -12,19 +13,77 @@ app.get("/", function (req, res) {
 
 // GET
 // GET ME ALL THE USERs
-app.get("/users");
+// http://localhost:4000/users
+app.get("/users", async (req, res) => {
+  const uri = "mongodb://localhost:27017";
+  const client = new MongoClient(uri);
+
+  let db = client.db("project");
+  let list = await db.collection("user").find().toArray();
+
+  await client.close();
+
+  res.json(list);
+});
 
 // GET
 // GET ME USER WHER ID IS PARAM
-app.get("/users/:id");
+app.get("/users/:id", async (req, res) => {
+  const uri = "mongodb://localhost:27017";
+  const client = new MongoClient(uri);
+
+  let db = client.db("project");
+
+  let id = new ObjectId(req.params.id);
+  let user = await db.collection("user").findOne({ _id: id });
+
+  await client.close();
+
+  res.json(user);
+});
+
+app.get("/users/query_by_username/:username", async (req, res) => {
+  const uri = "mongodb://localhost:27017";
+  const client = new MongoClient(uri);
+
+  let db = client.db("project");
+  let user = await db
+    .collection("user")
+    .findOne({ username: req.params.username });
+
+  await client.close();
+
+  res.json(user);
+});
 
 // POST
 // Create New User
 // http://localhost:4000/user
-app.post("/user/");
+app.post("/user/", async (req, res) => {
+  const uri = "mongodb://localhost:27017";
+  const client = new MongoClient(uri);
+
+  let userDocument = req.body;
+
+  const db = client.db("project");
+  await db.collection("user").insertOne(userDocument);
+
+  await client.close();
+
+  res.json({ opr: true });
+});
 
 // DELETE
 // DELETE ONE USER
-app.delete("/user/:id");
+app.delete("/user/:id", async (req, res) => {
+  const uri = "mongodb://localhost:27017";
+  const client = new MongoClient(uri);
+
+  const db = client.db("project");
+  let id = new ObjectId(req.params.id);
+  await db.collection("user").deleteOne({ _id: id });
+
+  await client.close();
+});
 
 app.listen(4000);
